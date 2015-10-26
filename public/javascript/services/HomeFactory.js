1bce6e45c83d5bf72eb17d2aaa98041298127cb3
LaurenLamborne <laurenlouann@gmail.com>
2015-10-26 18:25:28 -0400

    Lauren's add recipe commit

--- public/javascript/services/HomeFactory.js
+++ public/javascript/services/HomeFactory.js
@@ -2,11 +2,25 @@
 	'use strict';
 	angular.module('app')
 	.factory('HomeFactory', HomeFactory);
-
-
+	
 	function HomeFactory($http, $q) {
 		var o = {};
 
-		return o;
-	}
-})();
+		o.postRecipe = function(recipe) {
+			var q = $q.defer();
+			var i = 0;
+			while(i < recipe.ingredients.length)
+			{
+				if(!recipe.ingredients[i]) {
+					recipe.ingredients.splice(i, 1);
+				}
+				else i++;
+			}
+			if(recipe.ingredients.length < 1) q.reject(recipe);
+			else {
+				$http.post('/api/recipe', recipe).then(function(res) {
+					q.resolve(res);
+				});
+			}
+			return q.promise;
+		};
--- routes/RecipeRoutes.js
+++ routes/RecipeRoutes.js
@@ -2,9 +2,18 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Recipe = mongoose.model('Recipe');
-var jwt = require('express-jwt');
+// var jwt = require('express-jwt');
 
-
+router.post('/', function(req, res, next) {
+  var recipe = new Recipe(req.body);
+  // recipe.createdBy = req.payload._id;
+  recipe.created = new Date();
+  recipe.save(function(err, result) {
+    if(err) return next(err);
+    if(!result) return next("Could not Create");
+    res.send(result);
+  });
+});
 
 
 module.exports = router;
