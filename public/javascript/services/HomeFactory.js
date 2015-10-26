6e2120064d92c3b60262478e831358e0c70c024b
jhs458 <jhs458@gmail.com>
2015-10-26 15:50:13 -0700

    fixed home factory - john

--- public/javascript/services/HomeFactory.js
+++ public/javascript/services/HomeFactory.js
@@ -2,11 +2,6 @@
 	'use strict';
 	angular.module('app')
 	.factory('HomeFactory', HomeFactory);
-<<<<<<< HEAD
-
-=======
-	
->>>>>>> 1bce6e45c83d5bf72eb17d2aaa98041298127cb3
 	function HomeFactory($http, $q) {
 		var o = {};
 
@@ -18,9 +13,9 @@
 				if(!recipe.ingredients[i]) {
 					recipe.ingredients.splice(i, 1);
 				}
-				else i++;
+				else {i++;}
 			}
-			if(recipe.ingredients.length < 1) q.reject(recipe);
+			if(recipe.ingredients.length < 1) {q.reject(recipe);}
 			else {
 				$http.post('/api/recipe', recipe).then(function(res) {
 					q.resolve(res);
@@ -28,3 +23,7 @@
 			}
 			return q.promise;
 		};
+
+
+	}
+})();
